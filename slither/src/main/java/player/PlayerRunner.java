5b8f2b91282cdcb307bbf5620b40be54ebc6aefe
Kenton Van Peursem <kenton.vanpeursem@moremath.ai>
2022-03-15 09:14:29 -0500

    Rework

--- slither/src/main/java/player/PlayerRunner.java
+++ slither/src/main/java/player/PlayerRunner.java
@@ -2,6 +2,7 @@ package player;
 
 import slither.*;
 
+import java.lang.reflect.InvocationTargetException;
 import java.util.Random;
 
 import org.slf4j.*;
@@ -9,30 +10,56 @@ import org.slf4j.*;
 public class PlayerRunner {
     private static final Logger logger = LoggerFactory.getLogger(PlayerRunner.class);
 
-    public static void main(String[] args) {
+    private Random rand = new Random();
+    private int block;
+    private int boardSize;
+    private int games;
+    private Player player;
+
+    public PlayerRunner(int block, int boardSize, int games, Class<? extends Player> playerClazz) {
+        this.block = block;
+        this.boardSize = boardSize;
+        this.games = games;
+
+        player = playerFactory(playerClazz);
+    }
+
+    public SnakeConfig generateSnakeConfig() {
+        return new SnakeConfig(
+                50, boardSize * block, block, false, rand.nextInt(Integer.MAX_VALUE));
+    }
+
+    public int numGames() {
+        return games;
+    }
+
+    private Player playerFactory(Class<? extends Player> playerClazz) {
+        if (playerClazz == GreedyPlayer.class)
+                return new GreedyPlayer(rand.nextInt(Integer.MAX_VALUE));
+        // default brute force
+        return new BruteForcePlayer();
+    }
+
+    public int run() {
         GameController gameController = new GameController();
-        Random rand = new Random();
-
         int highestScore = -1;
-
-        var block = 25;
-        var boardSize = 21;
-
-        int games = 10;
-        int boardSeed = 42;
-        int playerSeed = 42;
-
         for (int i = 1; i <= games; i++) {
-            SnakeConfig config = new SnakeConfig(
-                    50, boardSize * block, block, false, rand.nextInt(Integer.MAX_VALUE));
-            // Player player = new GreedyPlayer(1535439173L);
-            Player player = new GreedyPlayer(rand.nextInt(Integer.MAX_VALUE));
-            int score = player.play(gameController, config);
+            int score = player.play(gameController, generateSnakeConfig());
 
             highestScore = Math.max(highestScore, score);
 
             logger.info("Game: {} Score: {} Highest {}",
                     i, score, highestScore);
         }
+
+        return highestScore;
+    }
+
+    public static void main(String[] args) {
+        // bug in bruteForce when boardSize is even number
+        // PlayerRunner runner = new PlayerRunner(25, 20, 1, BruteForcePlayer.class);
+
+        PlayerRunner runner = new PlayerRunner(25, 20, 1000, GreedyPlayer.class);
+        runner.run();
     }
 }
